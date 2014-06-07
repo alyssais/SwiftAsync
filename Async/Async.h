d45e6bc1e7109946bfafa379d590ae3226bc4f89
Alyssa Ross <hi@alyssa.is>
2014-06-07 19:04:55 +0100

    Initial Commit

---
+++ Async/Async.h
@@ -0,0 +1,19 @@
+//
+//  Async.h
+//  Async
+//
+//  Created by Alyssa Ross on 07/06/2014.
+//  Copyright (c) 2014 Alyssa Ross. All rights reserved.
+//
+
+#import <Cocoa/Cocoa.h>
+
+//! Project version number for Async.
+FOUNDATION_EXPORT double AsyncVersionNumber;
+
+//! Project version string for Async.
+FOUNDATION_EXPORT const unsigned char AsyncVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <Async/PublicHeader.h>
+
+
